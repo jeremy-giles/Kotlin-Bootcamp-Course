4155132871bf552ef29b0ae56f0a161266b4f5a0
GilesJ <gilesjeremydev@gmail.com>
2020-05-13 21:38:10 +0200

    Get Started

---
+++ src/getstarted/HelloKotlin.kt
@@ -0,0 +1,12 @@
+package getstarted
+
+// Type or paste the code below into the REPL
+// Tools -> Kotlin -> Kotlin REPL
+// And press play button or press Control+Enter (Command+Enter on a Mac) to run the code
+/*
+fun printHello() {
+    println("Hello World")
+}
+
+printHello()
+*/
